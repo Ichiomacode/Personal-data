5614ddf5780fa6a817b0003fb900a50f1465dcfa
unknown <ichiomafaustina@gmail.com>
2023-09-10 08:41:55 -0700

    latest commit

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-// Function to update the current day of the week
-function updateDayOfTheWeek() {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function updateCurrentDayOfTheWeek() {
+    const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    ];
     const currentDate = new Date();
-    const dayOfTheWeek = daysOfWeek[currentDate.getUTCDay()];
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = dayOfTheWeek;
-}
-
-// Function to update the current UTC time in milliseconds
-function updateUTCTime() {
-    const currentUTCTime = Date.now();
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
-}
-
-
-function updateUTCTimeEverySecond() {
-    setInterval(updateUTCTime, 1000); 
-}
-
-
-window.addEventListener('load', () => {
-    updateDayOfTheWeek();
-    updateUTCTimeEverySecond(); 
-});
+    const options = { weekday: 'long', timeZone: 'Africa/Lagos' }; // Nigeria's time zone
+    const currentDay = new Intl.DateTimeFormat('en-US', options).format(
+    currentDate
+    );
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    currentDay;
+    }
+    // Initial update
+    updateCurrentDayOfTheWeek();
+    // Update the current day of the week every 24 hours
+    setInterval(updateCurrentDayOfTheWeek, 86400000);
+    // Function to update the current local time in Nigeria (WAT time zone)
+    function updateCurrentLocalTime() {
+    const currentDate = new Date();
+    const options = { timeZone: 'Africa/Lagos' }; // Nigeria's time zone
+    const localTime = currentDate.toLocaleTimeString('en-US', options);
+    const utcTimeMilliseconds = currentDate.getTime(); // Get UTC time in milliseconds
+    document.querySelector('[data-testid="currentUTCTime"]').textContent =
+    'Local Time: ' + localTime + ' | UTC Time (ms): ' + utcTimeMilliseconds;
+    }
+    // Initial update
+    updateCurrentLocalTime();
+    // Update the current local time every second
+    setInterval(updateCurrentLocalTime, 1000);
