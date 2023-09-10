da2e459cf9e3e47ee821597ad62feb8d2b1bfa21
unknown <ichiomafaustina@gmail.com>
2023-09-10 12:59:55 -0700

    new

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-function updateCurrentDayOfTheWeek() {
-    const daysOfWeek = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-    ];
-    const currentDate = new Date();
-    const options = { weekday: 'long', timeZone: 'Africa/Lagos' }; // Nigeria's time zone
-    const currentDay = new Intl.DateTimeFormat('en-US', options).format(currentDate);
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
-}
+const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 
-// Initial update
-updateCurrentDayOfTheWeek();
+const myTime = function(){
+  const now = Date.now();
+  document.querySelector('[data-testid="currentUTCTime"]').innerHTML = now;
+};
 
-// Update the current day of the week every 24 hours
-setInterval(updateCurrentDayOfTheWeek, 86400000);
+setInterval(myTime,1000);
 
-function updateCurrentLocalTime() {
-    const utcTimeMilliseconds = Date.now(); // Get current UTC time in milliseconds
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = utcTimeMilliseconds.toString();
-}
-
-// Initial update
-updateCurrentLocalTime();
-
-// Update the current UTC time in milliseconds every second
-setInterval(updateCurrentLocalTime, 1000);
-
-
-// Initial update
-updateCurrentLocalTime()
+day.innerHTML = new Date().toLocaleString('en-US', {weekday: 'long'});
